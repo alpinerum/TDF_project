19a6b4f3dcb29a89c577ebdcd2081b5425307633
alpinerum <35735448+alpinerum@users.noreply.github.com>
2024-06-19 12:29:05 -0400

    created form

---
+++ src/app/tdf/tdf.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrl: './tdf.component.css'
+})
+export class TdfComponent {
+  email: string = '';
+  password: string = '';
+  gender: string = '';
+  age: number = 0;
+  onSubmit() {
+    console.log('Email ' + this.email);
+    console.log('Password ' + this.password);
+    console.log('Gender ' + this.gender);
+    console.log('Age ' + this.age);
+  }
+}
